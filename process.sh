6f005817c2dc869587d733a2ba7a8212a7eb8181
APigggggg <129783065+APigggggg@users.noreply.github.com>
2023-04-07 20:59:05 +0800

    Delete process.sh

--- process.sh
+++
@@ -1,8 +0,0 @@
-cp /mnt/c/SiYuan/data/widgets/temp/custom-dark.css  ./
-cp /mnt/c/SiYuan/data/widgets/temp/custom-light.css ./
-cp /mnt/c/SiYuan/data/widgets/temp/custom.css       ./
-
-python convert.py
-
-cp README.md main.js  manifest.json  ../siyuan-plugins/DarkPlusConcise 
-cp README.md main.js  manifest.json  /mnt/c/SiYuan/data/plugins/DarkPlusConcise
